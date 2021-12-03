4fa586b3cc69494c86288347dc373b18873216d6
sivanxpeer <sivan.x.peer@gmail.com>
2021-12-03 07:34:58 +0200

    guess my letter

--- guessMyLetter/index.js
+++ guessMyLetter/index.js
@@ -1,39 +1,73 @@
 
-
-//26 letters, pass 10 numbers 
-let randomLetter = (Math.random() + 1).toString(29).replace(/[^a-z]+/g, '').slice(0,1);
+//get a valid random letter
+// ---- 26 letters, pass 10 numbers , replace non abc with empty string
+let randomLetter = (Math.random()).toString(36).replace(/[^a-z]+/g, '').slice(0, 1);
 console.log("random", randomLetter);
-let guesses=["a","v","y","c"];
+
+let guesses = ["a", "v", "y", "c"];
 let gameOver = false;
 
-// let element =document.createElement()
-const letterEl= document.querySelector(".letter");
+const letterEl = document.querySelector(".letter");
 const messageEl = document.querySelector(".display");
-const keyGuessedEl = document.querySelector(".keys-guessed"); 
+const keysGuessedEl = document.querySelector(".keys-guessed");
 const guessesEl = document.querySelector(".guesses");
 
-// console.log(guessesEl.includes("a"));
 
 
-function display(message,color){
-    messageEl.innerText= message;
-    messageEl.style.color =color;
+function addButton() {
+    keysGuessedEl.style.display = "flex";
+    keysGuessedEl.style.flexDirection = "column";
+    const button = document.createElement("button");
+    button.innerText = "Yes";
+    button.style.width="50px";
+    keysGuessedEl.style.justifyContent="center";
+    keysGuessedEl.appendChild(button);
+}
+
+function display(message, color) {
+    messageEl.innerText = message;
+    messageEl.style.color = color;
+}
+
+function isValid(guess) {
+    if (guess.match(/[a-z]/i)) {
+        return true;
+    }
+    return false;
 }
 
 
-window.addEventListener("keydown",function(e){
-    if(e.key===randomLetter){
-        letterEl.innerHTML = e.key;
-        display("You guessed it!!","green");
+window.addEventListener("keydown", function (e) {
+    //check guess validity
+    if (!isValid(e.key))
+        display("please enter a valid letter!", "red");
 
+    //correct guess
+    else if (e.key === randomLetter) {
+        letterEl.innerHTML = e.key;
+        display("Right Letter!!", "green");
+        keysGuessedEl.innerHTML = "";
+        keysGuessedEl.innerHTML = "would you like to play again?";
+        addButton();
+        button.addEventListener("click",function(){
+            //reload page 
+        })
     }
 
+    //already tried the letter
+    else if(keysGuessedEl.innerText.includes(e.key)){
+        display(`${e.key} has already been guessed`);
+    }
+
+    //wrong guess
+    else {
+        display("nope, wrong letter", "red");
+        //make letter appear on same row but a row under the header
+        keysGuessedEl.innerHTML += "</br>" + e.key;
+    }
 
     console.log(e.key)
 
-    // else if{
-    //     e
-    // }a.
 })
 
 
