4b36eb33959e6c562bebc0a2f2dc2001f9e3eadc
sivanxpeer <sivan.x.peer@gmail.com>
2021-12-08 04:52:31 +0200

    heroes

---
+++ ex25.2.js
@@ -0,0 +1,13 @@
+obj = {
+    name: "Timmy",
+    printName: function(){
+        console.log(this.name);
+    },
+    printName2: function(){
+        setTimeout(this.printName.bind(this), 1000);
+        setTimeout(()=> console.log(this.name), 1000);
+    }
+}
+
+obj.printName();
+obj.printName2();
